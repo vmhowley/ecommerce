bf8c5b49e06b80517d58bc059448302a4f1ae2d4
vmhowley <vmhowleyh@gmail.com>
2023-09-12 16:51:59 -0400

    v0.1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './routes/Home'
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
